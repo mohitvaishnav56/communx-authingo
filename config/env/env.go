99b4d5e40d38fae8924fb9cdb5c43865d97ba016
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2026-05-03 07:26:24 +0530

    implemented some functions regarding env

--- config/env/env.go
+++ config/env/env.go
@@ -3,11 +3,12 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
-func load() {
+func Load() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("error loading env")
@@ -15,10 +16,42 @@ func load() {
 }
 
 func GetString(key string, fallback string) string {
-	load()
 	value, ok := os.LookupEnv(key)
 	if !ok {
 		return fallback
 	}
 	return value
+}
+
+
+func GetInt(key string, fallback int) int{
+	
+	value, ok := os.LookupEnv(key)
+	if !ok{
+		return fallback
+	}
+	
+	intValue, err := strconv.Atoi(value)
+	if err != nil{
+		fmt.Printf("error while conversion of env from string to int")
+		return fallback
+	}
+
+	return intValue
+}
+
+func GetBool(key string, fallback bool) bool{
+	
+	value, ok := os.LookupEnv(key)
+	if !ok{
+		return fallback
+	}
+	
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil{
+		fmt.Printf("error while conversion of env from string to int")
+		return fallback
+	}
+
+	return boolValue
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	config.Load()
 	addr := config.GetString("PORT", ":8080")
 	cfg := app.NewConfig(addr)
 	app := app.NewApplication(cfg)
